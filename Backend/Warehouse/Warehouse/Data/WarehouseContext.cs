07113827c5faf04bdbbf3f8eafb06a17a3a3108e
Daniel Toth <dantoth89@gmail.com>
2023-09-20 14:47:01 +0200

    Add - requirements for DB and Migration

---
+++ Backend/Warehouse/Warehouse/Data/WarehouseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Models.Entities;
+
+namespace Warehouse.Data;
+
+public class WarehouseContext : DbContext
+{
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public string DbPath { get; }
+    
+    private readonly IConfiguration Configuration; 
+    
+    public WarehouseContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        string connectionString = Configuration.GetConnectionString("WarehouseDb");
+        options.UseSqlServer(connectionString);
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Backend/Warehouse/Warehouse/Services/DatabaseManagementService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Data;
+
+namespace Warehouse.Services;
+
+public class DatabaseManagementService
+{
+    public static void MigrationInitialisation(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            serviceScope.ServiceProvider.GetService<WarehouseContext>().Database.Migrate();
+        }
+    }
+}
