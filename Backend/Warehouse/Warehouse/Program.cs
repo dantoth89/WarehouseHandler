87d3938cd8eeed54803519bf0d06bae5b6ec0012
Daniel Toth <dantoth89@gmail.com>
2023-09-21 15:15:54 +0200

    Add - Program Base

--- Backend/Warehouse/Warehouse/Program.cs
+++ Backend/Warehouse/Warehouse/Program.cs
@@ -1,6 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Warehouse.Services;
+using Warehouse.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<WarehouseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConfiguration"));
+});
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IInventoryService, IInventoryService>();
+builder.Services.AddScoped<ILocationService, LocationService>();
+builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+
+builder.Services.AddCors();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+DatabaseManagementService.MigrationInitialisation(app);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
