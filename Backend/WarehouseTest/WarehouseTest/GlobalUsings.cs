ddfd585f559c166b29e3f23918201ad778d2993b
Daniel Toth <dantoth89@gmail.com>
2023-09-28 17:32:11 +0200

    Add - Global usings for tests

---
+++ Backend/WarehouseTest/WarehouseTest/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using NUnit.Framework;
+global using Microsoft.EntityFrameworkCore;
+global using Warehouse.Data;
+global using Warehouse.Models.Entities;
+global using Warehouse.Services;
+global using Warehouse.Models.Enums;
