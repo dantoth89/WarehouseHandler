cda7a9c32eb22661f49851bb6e944cffc7af6c4b
Daniel Toth <dantoth89@gmail.com>
2023-10-02 13:41:24 +0200

    Add - Jwt service

---
+++ Backend/Warehouse/Warehouse/Services/JwtService.cs
@@ -0,0 +1,44 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Warehouse.Models.Entities;
+
+namespace Warehouse.Services;
+
+public class JwtService
+{
+    private readonly string _secretKey;
+    private readonly string _issuer;
+    private readonly string _audience;
+
+    public JwtService(string secretKey, string issuer, string audience)
+    {
+        _secretKey = secretKey;
+        _issuer = issuer;
+        _audience = audience;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            _issuer,
+            _audience,
+            claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: credentials
+        );
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+}
