f0ff2e48684e70d07b43857addbfdf19e115b4e3
Daniel Toth <dantoth89@gmail.com>
2023-10-03 16:47:14 +0200

    Add - User DTO

---
+++ Backend/Warehouse/Warehouse/Models/DTO/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using Warehouse.Models.Enums;
+
+namespace Warehouse.Models.DTO;
+
+public class UserDto
+{
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public Role Role { get; set; }
+}
