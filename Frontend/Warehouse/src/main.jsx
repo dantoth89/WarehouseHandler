8d1fe5c9d2fea20757d42e3d99c7020147ee5665
Daniel Toth <dantoth89@gmail.com>
2023-10-05 17:06:45 +0200

    Add - Frontend base

--- Frontend/Warehouse/src/main.jsx
+++ Frontend/Warehouse/src/main.jsx
@@ -1,18 +1,38 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Login from './pages/Login.jsx'
+import ProductList from './pages/ProductList.jsx'
+import Navbar from "./pages/Navbar.jsx";
+import SupplierList from "./pages/SupplierList.jsx"
+import LocationList from "./pages/LocationList.jsx";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
-  },  
-  ],
- );  
+  },
+  {
+    path: "/products",
+    element: <ProductList />,
+  },
+  {
+    path: "/suppliers",
+    element: <SupplierList />,
+  },
+  {
+    path: "/locations",
+    element: <LocationList />,
+  },
+],
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbar />{Navbar}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
