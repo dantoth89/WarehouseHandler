a30a687adde5fbc9c61682152e3746618f81caef
Daniel Toth <dantoth89@gmail.com>
2023-10-10 14:05:09 +0200

    Add - Product DTO

---
+++ Backend/Warehouse/Warehouse/Models/DTO/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Warehouse.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ProductDTO
+{
+    [MaxLength(255)]
+    public string Name { get; set; }
+
+    [MaxLength(50)]
+    public string SKU { get; set; }
+
+    [MaxLength(1000)]
+    public string Description { get; set; }
+    
+    public int SupplierId { get; set; }
+}
