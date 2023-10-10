10371ee59c8883cef6882a642e5ece3d11db9e06
Daniel Toth <dantoth89@gmail.com>
2023-10-10 14:04:57 +0200

    Add - Inventory DTO

---
+++ Backend/Warehouse/Warehouse/Models/DTO/InventoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Warehouse.Models.DTO;
+
+public class InventoryDTO
+{
+    public int ProductId { get; set; }
+
+    public string Location { get; set; }
+    
+    public int Quantity { get; set; }
+}
