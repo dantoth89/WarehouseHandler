9625a64d94578ec55acf934d21ca6a76ddad4f28
Daniel Toth <dantoth89@gmail.com>
2023-10-12 13:27:01 +0200

    Add - Location model

---
+++ Backend/Warehouse/Warehouse/Models/Entities/Location.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Warehouse.Models.Entities
+{
+    public class Location
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string LocationCode { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
