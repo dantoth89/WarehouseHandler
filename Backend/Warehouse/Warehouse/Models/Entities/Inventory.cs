e87adda3320782ac1e55d98921c9fe621ee07128
Daniel Toth <dantoth89@gmail.com>
2023-10-12 13:26:50 +0200

    Upd - Inventory model

--- Backend/Warehouse/Warehouse/Models/Entities/Inventory.cs
+++ Backend/Warehouse/Warehouse/Models/Entities/Inventory.cs
@@ -13,8 +13,9 @@ namespace Warehouse.Models.Entities
         public int ProductId { get; set; }
         
         public Product Product { get; set; }
-
-        public string Location { get; set; }
+        
+        [ForeignKey("Location")]
+        public int LocationId { get; set; }
         public int Quantity { get; set; }
     }
 }
