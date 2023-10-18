4d7e86fc4b07791ffbf0b6d68558d660f9ef511f
Daniel Toth <dantoth89@gmail.com>
2023-10-18 14:18:07 +0200

    Add - Order controller for test

---
+++ Backend/Warehouse/Warehouse/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Services;
+
+namespace Warehouse.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> MakeOrder([FromBody] List<int> invetoryList)
+    {
+        try
+        {
+            await _orderService.GenerateOrder(invetoryList);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    
+}
