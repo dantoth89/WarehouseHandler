ec7806fbc84e49e88b31185c7c211c340105bdd1
Daniel Toth <dantoth89@gmail.com>
2023-10-20 09:36:46 +0200

    Add - Order modell

---
+++ Backend/Warehouse/Warehouse/Models/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Warehouse.Models.Entities;
+
+public class Order
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public string OrderNotes { get; set; }
+    
+    public DateTime OrderDate { get; set; }
+    
+    public string OrderSummary { get; set; }
+}
